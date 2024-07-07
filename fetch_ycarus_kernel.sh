c6442c7e7bb70da1b16446ad0fb60fbe58230f51
Siegfried Kienzle <sikienzl.github@t-online.de>
2024-07-07 21:04:23 +0200

    Update fetch_ycarus_kernel.sh
    update kernel-version to 6.1.0 and also
    the kernel_package-version to 1.30

--- fetch_ycarus_kernel.sh
+++ fetch_ycarus_kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-KERNEL_VERSION="5.4.86"
-KERNEL_PACKAGE_VERSION="1.16+9d3f35b"
+KERNEL_VERSION="6.1.0"
+KERNEL_PACKAGE_VERSION="1.30"
 VPSURL="https://www.openmptcprouter.com/"
 KERNEL_RELEASE="${KERNEL_VERSION}-mptcp_${KERNEL_PACKAGE_VERSION}"
 wget -O /tmp/linux-image-${KERNEL_RELEASE}_amd64.deb ${VPSURL}kernel/linux-image-${KERNEL_RELEASE}_amd64.deb
